512f59a8b4ec9b893b347d86e02cf5bcf02abef9
Daniel Sachse <sachsedaniel@googlemail.com>
2012-06-20 18:53:43 +0200

    rewrote the taglib definition: There is a JSF spec issue which prevents to add the source tag (Resolved in JSF 2.2. The README now has the complete taglib definition for documentory purposes

--- src/main/java/at/w0mb/yabby/TaglibActivator.java
+++
@@ -1,9 +0,0 @@
-package at.w0mb.yabby;
-
-import javax.faces.component.FacesComponent;
-import javax.faces.component.UINamingContainer;
-
-@FacesComponent(value = "")
-public class TaglibActivator extends UINamingContainer {
-
-}
